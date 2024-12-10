d799907bc5fbd3645997d4d818eda9f7d6202972
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2024-12-10 11:27:54 +0100

    #3 Faire un modification sur les column et les types des colunm des table de database

--- ScriptDatabase/modifier.sql
+++ ScriptDatabase/modifier.sql
@@ -1,1 +1,15 @@
-/*rempler le le table login*/
+/* modifier les type choisis dans la table de ville pour le type des ville */
+ALTER TABLE ville
+MODIFY type ENUM('Capitale', 'Principale');
+    /* Exécute */
+
+/* modifier la langeur des varible nom de pays et ville  ,et la langeur de langues des pays */
+ALTER TABLE pays
+MODIFY nom VARCHAR(50);
+ALTER TABLE pays
+MODIFY langues VARCHAR(50);
+ALTER TABLE ville
+MODIFY nom VARCHAR(50);
+    /* Exécute */
+
+
